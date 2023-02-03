52bc1d971055a956f6e47631312354f3eaf527cc
Olivér Falvai <oliver.falvai@bitrise.io>
2023-02-03 12:25:00 +0100

    Initial step version

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+
+	"github.com/bitrise-io/go-steputils/v2/stepconf"
+	"github.com/bitrise-io/go-utils/v2/command"
+	"github.com/bitrise-io/go-utils/v2/env"
+	"github.com/bitrise-io/go-utils/v2/log"
+	"github.com/bitrise-steplib/steps-restore-carthage-cache/step"
+)
+
+func main() {
+	os.Exit(run())
+}
+
+func run() int {
+	logger := log.NewLogger()
+	envRepo := env.NewRepository()
+	inputParser := stepconf.NewInputParser(envRepo)
+	cmdFactory := command.NewFactory(envRepo)
+	cacheStep := step.New(logger, inputParser, envRepo, cmdFactory)
+
+	if err := cacheStep.Run(); err != nil {
+		logger.Errorf(err.Error())
+		return 1
+	}
+
+	return 0
+}
